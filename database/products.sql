aabc11f36dd8ee8995a8df53e91be84b09d0fb9d
Ezekiel Santos <ezekielsantos.dev@gmail.com>
2024-10-02 07:08:19 +0800

    added new database structure

--- database/products.sql
+++ database/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Oct 01, 2024 at 10:29 AM
+-- Generation Time: Oct 01, 2024 at 11:07 PM
 -- Server version: 9.0.1
 -- PHP Version: 8.2.8
 
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `addresses`
+--
+
+CREATE TABLE `addresses` (
+  `address_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `address_line1` varchar(255) DEFAULT NULL,
+  `address_line2` varchar(255) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(20) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orders`
 --
 
@@ -35,6 +53,17 @@ CREATE TABLE `orders` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `total_price`, `order_status`, `created_at`, `updated_at`) VALUES
+(1, 24, 350.00, 'pending', '2024-10-01 13:26:10', '2024-10-01 13:26:10'),
+(2, 24, 200.00, 'pending', '2024-10-01 13:28:37', '2024-10-01 13:28:37'),
+(3, 24, 400.00, 'pending', '2024-10-01 19:33:53', '2024-10-01 19:33:53'),
+(4, 24, 200.00, 'pending', '2024-10-01 19:34:31', '2024-10-01 19:34:31'),
+(5, 24, 90.00, 'pending', '2024-10-01 19:34:46', '2024-10-01 19:34:46');
 
 -- --------------------------------------------------------
 
@@ -48,6 +77,32 @@ CREATE TABLE `order_items` (
   `product_id` int NOT NULL,
   `quantity` int NOT NULL DEFAULT '1',
   `product_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`order_item_id`, `order_id`, `product_id`, `quantity`, `product_price`) VALUES
+(1, 1, 7, 7, 50.00),
+(2, 2, 19, 12, 40.00),
+(3, 3, 16, 8, 50.00),
+(4, 4, 17, 8, 25.00),
+(5, 5, 18, 5, 30.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `payment_id` int NOT NULL,
+  `order_id` int NOT NULL,
+  `payment_method` varchar(50) DEFAULT NULL,
+  `payment_status` enum('pending','completed','failed') DEFAULT 'pending',
+  `transaction_id` varchar(255) DEFAULT NULL,
+  `payment_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -116,6 +171,7 @@ CREATE TABLE `users` (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mobile_number` varchar(11) DEFAULT NULL,
   `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -125,20 +181,27 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_image`, `email`, `password`, `first_name`, `last_name`, `role`, `status`, `created_at`) VALUES
-(17, '1727036240591_17.jpg', 'gambino@email.test', '$2y$10$s9OSY8YFNX.QEaIppuS4eOmaHPLuvFOr/7F6YRLzcLV3PMhayaSJK', 'Childish', 'Gambino', 'customer', 'inactive', '2024-09-22 01:54:58'),
-(20, '1727036277926_20.jpg', 'admin@email.test', '$2y$10$FtXQYkNL6jc8DhilcwpAT.BV2Oobbm0nS09UuXGBcR2g9fpsPUY3C', 'admin', 'admin', 'administrator', 'active', '2024-09-22 02:05:19'),
-(21, '1727036240591_17.jpg', 'roger@email.test', '$2y$10$zpwIFFAQNGEWUu3eEVIwc.hodej4q0Hx/cWUlKLRtDD7ASMKpwtiW', 'Roger', 'Baker', 'customer', 'active', '2024-09-22 20:41:19'),
-(22, '1727036240591_17.jpg', 'paulo@email.test', '$2y$10$jIDD4Xw7v23K6/r0beQYB.37O5I3DhRbV3oenIt5ov..5IvZynkzu', 'Paulo', 'Banchero', 'customer', 'active', '2024-09-22 20:41:42'),
-(24, '1727036240591_17.jpg', 'ej@email.test', '$2y$10$/n72EBflqbOFhabdH7/MQujlXytNdAXGDQiwA4pouEUvvDL5OiSnq', 'Ej', 'Santos', 'customer', 'active', '2024-09-22 20:51:46'),
-(25, '1727036240591_17.jpg', 'lisa@email.test', '$2y$10$Vy.EbR1h3uEJp0IeS27RVeDXBua0Of7XXS6urVEfQ4/a460ZCaBiO', 'Lisa', 'Manoban', 'customer', 'active', '2024-09-22 20:54:01'),
-(26, '1727036240591_17.jpg', 'sheena@email.test', '$2y$10$nCUgkOx7fGyyEFNIvb6SwOTncYbzxrWWZiJuPwlwX9Gcaj.SdO1IS', 'Sheena', 'Bini', 'customer', 'active', '2024-09-22 20:55:17'),
-(27, '1727036240591_17.jpg', 'paul@email.test', '$2y$10$fop3FA/zAR6BrMcQrBiT2uErbflRdTc3/L7eiE4Ib0RfAbkgzGuKG', 'Paul', 'Wasabi', 'customer', 'active', '2024-09-22 20:55:52'),
-(28, '1727036240591_17.jpg', 'david@email.com', '$2y$10$K.xzAArl9daVAnJzqHgUEOyGnHeEcMWKP2jGsxRDVuf0/VlbLbCge', 'David', 'Bowie', 'customer', 'active', '2024-09-22 20:56:16');
+INSERT INTO `users` (`user_id`, `user_image`, `email`, `password`, `first_name`, `last_name`, `mobile_number`, `role`, `status`, `created_at`) VALUES
+(17, '1727036240591_17.jpg', 'gambino@email.test', '$2y$10$s9OSY8YFNX.QEaIppuS4eOmaHPLuvFOr/7F6YRLzcLV3PMhayaSJK', 'Childish', 'Gambino', NULL, 'customer', 'inactive', '2024-09-22 01:54:58'),
+(20, '1727036277926_20.jpg', 'admin@email.test', '$2y$10$FtXQYkNL6jc8DhilcwpAT.BV2Oobbm0nS09UuXGBcR2g9fpsPUY3C', 'admin', 'admin', NULL, 'administrator', 'active', '2024-09-22 02:05:19'),
+(21, '1727036240591_17.jpg', 'roger@email.test', '$2y$10$zpwIFFAQNGEWUu3eEVIwc.hodej4q0Hx/cWUlKLRtDD7ASMKpwtiW', 'Roger', 'Baker', NULL, 'customer', 'active', '2024-09-22 20:41:19'),
+(22, '1727036240591_17.jpg', 'paulo@email.test', '$2y$10$jIDD4Xw7v23K6/r0beQYB.37O5I3DhRbV3oenIt5ov..5IvZynkzu', 'Paulo', 'Banchero', NULL, 'customer', 'active', '2024-09-22 20:41:42'),
+(24, '1727036240591_17.jpg', 'ej@email.test', '$2y$10$/n72EBflqbOFhabdH7/MQujlXytNdAXGDQiwA4pouEUvvDL5OiSnq', 'Ej', 'Santos', NULL, 'customer', 'active', '2024-09-22 20:51:46'),
+(25, '1727036240591_17.jpg', 'lisa@email.test', '$2y$10$Vy.EbR1h3uEJp0IeS27RVeDXBua0Of7XXS6urVEfQ4/a460ZCaBiO', 'Lisa', 'Manoban', NULL, 'customer', 'active', '2024-09-22 20:54:01'),
+(26, '1727036240591_17.jpg', 'sheena@email.test', '$2y$10$nCUgkOx7fGyyEFNIvb6SwOTncYbzxrWWZiJuPwlwX9Gcaj.SdO1IS', 'Sheena', 'Bini', NULL, 'customer', 'active', '2024-09-22 20:55:17'),
+(27, '1727036240591_17.jpg', 'paul@email.test', '$2y$10$fop3FA/zAR6BrMcQrBiT2uErbflRdTc3/L7eiE4Ib0RfAbkgzGuKG', 'Paul', 'Wasabi', NULL, 'customer', 'active', '2024-09-22 20:55:52'),
+(28, '1727036240591_17.jpg', 'david@email.com', '$2y$10$K.xzAArl9daVAnJzqHgUEOyGnHeEcMWKP2jGsxRDVuf0/VlbLbCge', 'David', 'Bowie', NULL, 'customer', 'active', '2024-09-22 20:56:16');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`address_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `orders`
@@ -154,6 +217,13 @@ ALTER TABLE `order_items`
   ADD PRIMARY KEY (`order_item_id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indexes for table `products`
@@ -179,16 +249,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `address_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `order_item_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `order_item_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -213,6 +295,12 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
@@ -224,6 +312,12 @@ ALTER TABLE `orders`
 ALTER TABLE `order_items`
   ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
   ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 
 --
 -- Constraints for table `shipping_details`
